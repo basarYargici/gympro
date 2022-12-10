c0c74bd7043c5cfc5bb377ce3f89703161b68407
basarYargici <ibrahimbyargici@gmail.com>
2022-12-10 03:05:59 +0300

    add qr refresh logic

--- lib/pages/after_login/qr_page.dart
+++ lib/pages/after_login/qr_page.dart
@@ -1,5 +1,5 @@
+import 'package:circular_countdown/circular_countdown.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_countdown_timer/index.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
 class QrScreen extends StatefulWidget {
@@ -12,65 +12,76 @@ class QrScreen extends StatefulWidget {
 class _QrScreenState extends State<QrScreen> {
   final String image =
       "https://www.expatica.com/app/uploads/sites/10/2014/05/best-place-to-live-in-uk.jpg";
-  late CountdownTimerController controller;
-  int? endTime = DateTime.now().millisecondsSinceEpoch + 1000 * 30;
-
-  @override
-  void initState() {
-    super.initState();
-    controller = CountdownTimerController(endTime: endTime ?? 0, onEnd: onEnd);
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    endTime = null;
-    super.dispose();
-  }
-
-  void onEnd() {
-    print('onEnd');
-  }
+  bool isTimerFinished = false;
+  int countDownTotal = 3;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(
-                right: 8.0,
-                left: 8.0,
-                top: 20.0,
+      body: Padding(
+        padding: const EdgeInsets.only(top:40.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  right: 8.0,
+                  left: 8.0,
+                  top: 20.0,
+                ),
+                child: titleText(),
               ),
-              child: titleText(),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                right: 8.0,
-                left: 8.0,
-                bottom: 24.0,
+              Padding(
+                padding: const EdgeInsets.only(
+                  right: 8.0,
+                  left: 8.0,
+                  bottom: 24.0,
+                ),
+                child: descriptionText(),
               ),
-              child: descriptionText(),
-            ),
-            qrCard(context),
-            Container(
-              alignment: Alignment.bottomCenter,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: refreshButton(),
-              ),
-            ),
-            Center(
-              child: CountdownTimer(
-                controller: controller,
-                onEnd: onEnd,
-                endTime: endTime,
-              ),
-            ),
-          ],
+              qrCard(context),
+              circularCountdown()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget circularCountdown() {
+    if (isTimerFinished) {
+      return Container(
+        alignment: Alignment.bottomCenter,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: refreshButton(),
+        ),
+      );
+    }
+    return Padding(
+      padding: const EdgeInsets.only(top:32.0),
+      child: Center(
+        child: TimeCircularCountdown(
+          onCanceled: (unit, remaining) {
+            print("cancelled");
+            isTimerFinished = true;
+            countDownTotal = 3;
+            setState(() {});
+          },
+          countdownTotalColor: Colors.orange,
+          countdownRemainingColor: Colors.transparent,
+          unit: CountdownUnit.second,
+          countdownTotal: countDownTotal,
+          onUpdated: (unit, remainingTime) => print('Updated'),
+          textStyle: const TextStyle(
+            color: Colors.orange,
+            fontSize: 70,
+          ),
+          onFinished: () {
+            isTimerFinished = true;
+            setState(() {});
+          },
         ),
       ),
     );
@@ -80,8 +91,9 @@ class _QrScreenState extends State<QrScreen> {
     return Center(
       child: ElevatedButton(
         onPressed: () {
-          onEnd();
-          controller.disposeTimer();
+          isTimerFinished = false;
+          countDownTotal = 3;
+          setState(() {});
         },
         child: const Text('Click to refresh The QR'),
       ),
@@ -112,27 +124,53 @@ class _QrScreenState extends State<QrScreen> {
 
   Center qrCard(BuildContext context) {
     return Center(
-      child: Container(
-        width: MediaQuery.of(context).size.width * 0.8,
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-          // color: const Color.fromARGB(0, 10, 246, 30),
-          borderRadius: BorderRadius.circular(20),
-          border: Border.all(color: Colors.blueAccent),
-        ),
-        child: Card(
-          elevation: 0,
-          child: qrCode(),
-        ),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width * 0.8,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              // color: const Color.fromARGB(0, 10, 246, 30),
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(color: Colors.blueAccent),
+            ),
+            child: Card(
+                elevation: 0,
+                child: (isTimerFinished) ? qrCodeError() : qrCode()),
+          ),
+        ],
       ),
     );
   }
 
-  QrImage qrCode() {
+  Widget qrCode() {
     return QrImage(
       data: "1234567890",
       version: QrVersions.auto,
       foregroundColor: Colors.black,
+    );
+  }
+
+  Widget qrCodeError() {
+    return Column(
+      // ignore: prefer_const_literals_to_create_immutables
+      children: [
+        const Icon(
+          Icons.error,
+          color: Colors.black,
+          size: 300,
+        ),
+        const Text(
+          "Current Time Expired. \n You should scan the QR before the expiration time!",
+          softWrap: true,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
+        )
+      ],
     );
   }
 }
