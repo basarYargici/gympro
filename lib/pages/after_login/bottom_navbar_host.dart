245fe5619f65d625b29a970bc75fc4d94e65b231
basarYargici <ibrahimbyargici@gmail.com>
2022-12-11 01:53:14 +0300

    add drawer

--- lib/pages/after_login/bottom_navbar_host.dart
+++ lib/pages/after_login/bottom_navbar_host.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gym_pro/pages/after_login/qr_page.dart';
 import 'package:gym_pro/pages/after_login/notify_page.dart';
 import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 
+import '../../firebase_helper.dart';
+import 'body_model_form_page.dart';
 import 'home_page.dart';
 
 class BottomNavBarHost extends StatefulWidget {
@@ -15,8 +18,16 @@ class BottomNavBarHost extends StatefulWidget {
 
 class _BottomNavBarHostState extends State<BottomNavBarHost> {
   PersistentTabController? controller;
+  late final User? user;
+
   bool _hideAppBar = false;
   String title = "Dashboard";
+
+  @override
+  void initState() {
+    user = FirebaseHelper().currentUser;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,6 +62,19 @@ class _BottomNavBarHostState extends State<BottomNavBarHost> {
 
     return Scaffold(
       appBar: appBar(),
+      drawer: Drawer(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+            child: Column(
+              children: [
+                buildHeader(context),
+                buildMenuItems(context),
+              ],
+            ),
+          ),
+        ),
+      ),
       body: PersistentTabView(
         context,
         controller: controller,
@@ -95,6 +119,92 @@ class _BottomNavBarHostState extends State<BottomNavBarHost> {
     );
   }
 
+  Container buildHeader(BuildContext context) {
+    return Container(
+      color: Colors.orange,
+      width: double.infinity,
+      child: Column(children: [
+        const SizedBox(
+          height: 12,
+        ),
+        const CircleAvatar(
+          radius: 52,
+          backgroundImage: NetworkImage(
+              'https://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?cs=srgb&dl=pexels-suliman-sallehi-1704488.jpg&fm=jpg'),
+        ),
+        const SizedBox(
+          height: 12,
+        ),
+        const Text(
+          'ASDASD',
+          style: TextStyle(fontSize: 28, color: Colors.white),
+        ),
+        Text(
+          user?.email ?? '',
+          style: const TextStyle(fontSize: 16, color: Colors.white),
+        ),
+        const SizedBox(
+          height: 12,
+        ),
+      ]),
+    );
+  }
+
+  Widget buildMenuItems(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 16,
+      ),
+      child: Wrap(
+        runSpacing: 8,
+        children: [
+          ListTile(
+            leading: const Icon(Icons.home),
+            title: const Text("data"),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const BodyModelFormPage(),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.home),
+            title: const Text("data"),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.home),
+            title: const Text("data"),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.home),
+            title: const Text("data"),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.home),
+            title: const Text("data"),
+            onTap: () {},
+          ),
+          const Divider(
+            color: Colors.black54,
+          ),
+          ListTile(
+            leading: const Icon(Icons.home),
+            title: const Text("data"),
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+
   PreferredSizeWidget? appBar() {
     if (_hideAppBar) {
       return null;
@@ -102,7 +212,6 @@ class _BottomNavBarHostState extends State<BottomNavBarHost> {
     return AppBar(
       backgroundColor: Colors.transparent,
       elevation: 0,
-      leading: const Icon(Icons.menu),
       title: Text(title),
     );
   }
